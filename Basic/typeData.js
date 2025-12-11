96cf75a8ed8bb5089b524bbfa9f9f5fb006e3152
MrPrasetyo <prasetyo1103id@gmail.com>
2025-12-12 05:48:28 +0700

    Data types

---
+++ Basic/typeData.js
@@ -0,0 +1,8 @@
+const dataInteger = 12345;
+const dataNumber = 3.14;
+const dataString = "This is an example of String type of Data";
+const dataNull = null;
+const dataBoolean = true;
+let dataUndefined;
+
+console.log(dataBoolean) // put any of Variabel above to show in console log how each type of data shown in log.
